c2829b9c838156f86de1b88ba04a5f941927ea5b
Marzan1810662 <hassanmarzan17@gmail.com>
2022-06-05 08:34:31 +0600

    dragstart and dragend event added to the boxes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 console.log('script.js connected');
+
+// console.log(document.getElementsByClassName('box'));
+// console.log(document.querySelectorAll('.box'));
+
+const columns = document.getElementsByClassName('column');
+const boxes = document.getElementsByClassName('box');
+
+console.log(columns);
+console.log(boxes);
+
+for (const box of boxes) {
+    //when the drag events starts a class 'dragging' will be added to the dragged div
+    box.addEventListener('dragstart', () => {
+        console.log('drag start');
+        box.classList.add('dragging');
+    })
+    //when the drag events ends the class will be removed from the dragged div
+    box.addEventListener('dragend', () => {
+        console.log('drag end');
+        box.classList.remove('dragging');
+
+    })
+}
